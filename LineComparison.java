f0dcb9c560dfe317154a3b61292ca72ecd3b1c8e
arshivam <shivam.armarkar007@gmail.com>
2022-01-19 11:20:22 +0530

    added welcome program

---
+++ LineComparison.java
@@ -0,0 +1,8 @@
+package Lineproblems;
+
+public class LineComparison {
+	 public static void main(String[] args) {
+	   System.out.println("Welcome to Line Comparision Computation Program");
+	 }
+
+}
